c72b6ee95cc2fbc2a0233cd3abfe18d18f509464
Kiran Raut <kiranraut04196@gmail.com>
2025-12-25 08:33:48 +0530

    test 2 change utc timing

--- src/app/api/hello/route.ts
+++ src/app/api/hello/route.ts
@@ -1,4 +1,10 @@
 export const GET = () => {
-    console.log("inside the route")
-    return new Response("Hello, Next.js!");
+    try {
+        console.log("inside the route")
+        
+        return Response.json({ success: true,  message: "Hello world" }, { status: 200 });
+    } catch (error) {
+        console.error(error);
+        return Response.json({ success: false, message: "Error", error: error }, {status: 500});
+    }
 };
